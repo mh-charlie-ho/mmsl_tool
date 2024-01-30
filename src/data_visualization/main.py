dc5e49ed54cf5250637aec7aa35bec958c26ceee
Ho, Ming-Hsin <charlie.ho@itri.org.tw>
2024-01-30 13:46:01 +0800

    version1-slice-data-profile

--- src/data_visualization/main.py
+++ src/data_visualization/main.py
@@ -1,53 +1,12 @@
 #!/usr/bin/env python
-import ros_node
-import show_data_final as sdf
+import slice_data_profile
+
 from itri_msgs.msg import GroundFilterColData
 
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-
-import sys
-
-
-def Update(frame, axList):
-    print(len(axList))
-
-    tmp = []
-    for ax in axList:
-        print(ax.GetDataFrame())
-        print(ax.GetRowData())
-        input()
-        ax.UpdateData(frame)
-        print(ax.mX, ax.mY)
-        tmp.append(ax.GetAxesObj())
-
-    return tmp
-
 if __name__ == '__main__':
-    topicName = ["col_pts"]
-    typeName = [GroundFilterColData]
-    
-    node = ros_node.RosNode("node_name")
-    dataList = node.Receiver(topicName, typeName)
-    
-    axList = []
-    pointColor = ['blue', 'red']
-    
-    print(dataList)
+    # For Test =================================================================
+    topicName = ["/col_pts", "/col_floating_obs_pts"]
+    typeName = [GroundFilterColData, GroundFilterColData]
+    # ==========================================================================
 
-    fig, ax = plt.subplots()
-    for i in range(len(dataList)):
-        axList.append(sdf.AnimationAxesData(
-            ax, dataList[i], pointColor[i]))  # rowEndNum=10
-    
-    print(len(axList))
-
-    if len(axList) == 0:
-        sys.exit()
-
-    ani = FuncAnimation(fig, Update, fargs=(axList,), frames=range(100), interval=100)
-    plt.show()
-
-
-    
-    
+    slice_data_profile.Work(topicName, typeName)
---
+++ src/data_visualization/organize_msg.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+import pandas as pd
+from data_points import DataPoints
+
+
+class ManageMsg(DataPoints):
+
+    def __init__(self, msgData, rowEndId=None):
+        super(ManageMsg, self).__init__(
+            msgData["range"],
+            msgData["height"],
+            msgData["rowid"],
+            msgData["type"],
+        )
+
+        self.__SetOrderFrame(rowEndId) # self.__mOrderFrame
+    
+    def __SetOrderFrame(self, rowEndId):
+        dataFrame = pd.DataFrame({
+            'rowid': self.mRowIds,
+            'x': self.mXs,
+            'y': self.mYs,
+            'type': self.mTypes
+        })
+
+        existingRowid = dataFrame['rowid'].values
+
+        if rowEndId is None:
+            rowEndId = max(existingRowid)
+
+        missingRows = set(range(rowEndId)) - set(existingRowid)
+
+        subDataFrame = pd.DataFrame({
+            'rowid': list(missingRows),
+            'x': 0, 
+            'y': 0, 
+            'type': 0
+        })
+
+        newDataFrame = pd.concat([dataFrame, subDataFrame], ignore_index=True)
+        self.__mOrderFrame = newDataFrame.sort_values(
+            ['rowid', 'x'], ascending=True).reset_index(drop=True)
+    
+    def GetRowData(self, rowId=None, index=None):
+
+        if rowId is None:
+            return self.__mOrderFrame
+        
+        if rowId not in self.__mOrderFrame['rowid'].values:
+            return self.__mOrderFrame.loc[len(self.__mOrderFrame)-1]
+        
+        dataFrame = (self.__mOrderFrame[
+            self.__mOrderFrame['rowid']==rowId]).reset_index(drop=True)   
+           
+        if index is None:
+            return dataFrame
+        
+        index = index \
+            if index <= len(dataFrame) else len(dataFrame)-1
+        
+        return dataFrame.loc[index]
+    
+    def GetCol(self, formatData, colnumName):
+        '''
+        TBD
+        Check the col name is existing. 
+        '''
+        return formatData[str(colnumName)]
+
+
+if __name__ == '__main__':
+    testDict = {
+        "rowid":  [0, 0, 0, 0, 0, 0, 0, 0],
+        "type":   [  0,   0,   0,   0,   0,   0,   0,   0],
+        "range":  [ 0.000000, 1.766293, 2.445991, 3.195587, 3.602719, 3.911899,
+                    4.134902, 4.402998],
+        "height": [ 0.000000, 26.402569, 86.429100, 26.110262, 26.780521,
+                   27.411665, 35.315552, 27.092993]
+    }
+
+    testObj = ManageMsg(testDict)
+    print(testObj.GetRowData())
+    print(" ")
+    # print(testObj.GetRowData(rowId=2))
+    # pandas = testObj.GetRowData(rowId=2)
+    # print(testObj.GetCol(pandas, "x"))
+    # print(" ")
+    # print(testObj.GetRowData(rowId=2, index=3))
+
+    # series = testObj.GetRowData(rowId=2, index=3)
+    # print(testObj.GetCol(series, "x"))
+
+
+    #    rowid  type         x          y
+    # 0    0.0     0  0.000000   0.000000
+    # 1    0.0     0  1.766293  26.402569
+    # 2    0.0     0  2.445991  86.429100
+    # 3    0.0     0  3.195587  26.110262
+    # 4    0.0     0  3.602719  26.780521
+    # 5    0.0     0  3.911899  27.411665
+    # 6    0.0     0  4.134902  35.315552
+    # 7    0.0     0  4.402998  27.092993
+
+    table = pd.DataFrame({
+        "rowid": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        "type":  [  0,   0,   0,   0,   0,   0,   0,   0],
+        "x":[0.000000,
+                1.766293,
+                2.445991,
+                3.195587,
+                3.602719,
+                3.911899,
+                4.134902,
+                4.402998],
+        "y": [ 0.000000,
+                26.402569,
+                86.429100,
+                26.110262,
+                26.780521,
+                27.411665,
+                35.315552,
+                27.092993]
+    })
